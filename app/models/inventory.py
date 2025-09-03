b50082499239b4d8b7639688a8b90c251d1cbed2
swapno963 <swapno963@gmail.com>
2025-09-03 23:52:01 +0600

    bug fixed

--- app/models/inventory.py
+++ app/models/inventory.py
@@ -2,8 +2,10 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 import enum
-
+from typing import Optional
+from pydantic import BaseModel
 from app.db.postgresql import Base
+
 
 # Enum for action_type
 class ActionType(enum.Enum):
@@ -11,19 +13,23 @@ class ActionType(enum.Enum):
     REMOVE = "remove"
     UPDATE = "update"
 
+
 class Chemical(Base):
     __tablename__ = "chemicals"
 
-    id = Column(Integer, primary_key=True,  index=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False, unique=True)
-    cas_number = Column(String(50), nullable=False, unique=True)   
+    cas_number = Column(String(50), nullable=False, unique=True)
     quantity = Column(Float, nullable=False, default=0.0)
     unit = Column(String(50), nullable=False)  # e.g., g, L, ml, kg
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False
+    )
 
     # Relationship to InventoryLog
     logs = relationship("InventoryLog", back_populates="chemical")
+
 
 class InventoryLog(Base):
     __tablename__ = "inventory_logs"
@@ -36,8 +42,6 @@ class InventoryLog(Base):
 
     # Relationship back to Chemical
     chemical = relationship("Chemical", back_populates="logs")
-
-
 
 
 class ChemicalUpdate(BaseModel):

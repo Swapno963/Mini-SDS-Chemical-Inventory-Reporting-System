1d3b08ff704f050413311c60a6fac02779a52420
swapno963 <swapno963@gmail.com>
2025-09-04 21:17:45 +0600

    Alembic migration will wait for posgress to start. and it will wait for 2 seconds untill the postgress starts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 set -e
 
+
+
+# Wait for Postgres to be ready
+echo "Waiting for Postgres to start..."
+while ! pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER > /dev/null 2>&1; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+
+
 # Run Alembic migrations
 echo "Running Alembic migrations..."
 alembic upgrade head
 
 # Start FastAPI app
-echo "Starting FastAPI..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000

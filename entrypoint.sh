9df383e94c5ef1932d77b50b1b9482b8a817f54b
swapno963 <swapno963@gmail.com>
2025-09-06 14:13:28 +0600

    Project will run on localhost insteds of 0.0.0.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,4 +22,4 @@ echo "Running Alembic migrations..."
 alembic upgrade head
 
 # Start FastAPI app
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host localhost --port 8000

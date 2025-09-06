dbfa5a56af0c7da0533960359751540ab5a6a4d2
swapno963 <swapno963@gmail.com>
2025-09-06 14:02:01 +0600

    Now checking databse readyness by untill insted of while loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,12 @@ set -e
 
 # Wait for Postgres to be ready
 echo "Waiting for Postgres to start..."
-while ! pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER > /dev/null 2>&1; do
+echo "DB_HOST: $DB_HOST"
+echo "DB_PORT: $DB_PORT"
+echo "DB_USER: $DB_USER"
+export PGPASSWORD=$DB_PASSWORD
+
+until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" ; do
   echo "Postgres is unavailable - sleeping"
   sleep 2
 done
